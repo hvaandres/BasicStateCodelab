48c0242eca336fb1c27e78ec9057a203d377be10
hvaandres <hvaandres@icloud.com>
2023-03-22 23:40:18 -0600

    Initial commit

---
+++ app/src/main/java/com/example/basicstatecodelab/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.basicstatecodelab
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.basicstatecodelab.ui.theme.BasicStateCodelabTheme
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.ui.unit.dp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BasicStateCodelabTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    WellnessScreen()
+                }
+            }
+        }
+    }
+}
+
